d117c0acc90f23eb3087b47ba9fbd987b343dba6
milleaus <naubol@gmail.com>
2016-06-14 15:51:06 -0400

    added pretty print to json

--- src/main/scala/aug/util/Util.scala
+++ src/main/scala/aug/util/Util.scala
@@ -29,6 +29,10 @@ object JsonUtil {
     mapper.writeValueAsString(value)
   }
 
+  def prettyJson(value: Any): String = {
+    mapper.writerWithDefaultPrettyPrinter().writeValueAsString(value)
+  }
+
   def toMap[V](json:String)(implicit m: Manifest[V]) = fromJson[Map[String,V]](json)
 
   def fromJson[T](json: String)(implicit m : Manifest[T]): T = {
