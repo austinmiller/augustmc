b471d894c43fbd995d21d31a17273fc82527f16a
Austin Miller <naubol@gmail.com>
2017-07-17 10:57:08 -0400

    added date string to slog

--- src/main/scala/aug/io/SystemLog.scala
+++ src/main/scala/aug/io/SystemLog.scala
@@ -1,8 +1,13 @@
 package aug.io
 
+import java.text.SimpleDateFormat
+import java.util.Date
+
 import aug.gui.SystemPanel
 
 class SystemLog(systemPanel: SystemPanel) {
+
+  val dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
 
   def raw(msg: String) = {
     systemPanel.text.addText(msg)
@@ -10,7 +15,8 @@ class SystemLog(systemPanel: SystemPanel) {
 
   def info(msg: String, args: Object*) = {
     val m = String.format(msg, args)
-    val txt = "\n" + ColorUtils.colorCode("37") + "INFO: " + ColorUtils.colorCode("0") + msg
+    val txt = "\n" + ColorUtils.colorCode("37") +
+      dateFormat.format(new Date) + " INFO: " + ColorUtils.colorCode("0") + msg
     systemPanel.text.addText(txt)
     systemPanel.repaint()
   }
